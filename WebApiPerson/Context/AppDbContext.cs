4c4a99d56705e1df1d02305f9ca2f34076440e5c
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-04-24 01:55:28 -0300

    Agregar archivos de proyecto.

---
+++ WebApiPerson/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context
+{
+    public class AppDbContext: DbContext
+    {   
+        // Constructor de la clase AppDbContext que recibe un objeto de tipo DbContextOptions<AppDbContext> y lo pasa al constructor de la clase base DbContext.
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        // DbSet es una colección de entidades que se pueden consultar, modificar y guardar en la base de datos.
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ WebApiPerson/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Creamos variable para la cadena de conexión
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Añadimos el contexto de la base de datos a la aplicación
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
